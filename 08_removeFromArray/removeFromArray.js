9ed53e8f207dcc97e0f803c89b3b4cca9f07118d
abdul salaam <asalaam75555@gmail.com>
2025-07-17 16:10:54 +0000

    Add changes to complete 08 assignment of TOP JS

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+    let arr1 = []
+    let arr2 = []
+    let result = [] 
+    if (args.length > 0){
+        arr1 = args[0];
+    }
+    if (args.length > 1){
+        arr2 = args.slice(1);
+    }
+    for (let i of arr2){
+                arr1 = arr1.filter(item => {
+                    if(item !== i)
+                    {
+                         return item;
+                    }})
+            }   
+    return arr1; 
+   
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
