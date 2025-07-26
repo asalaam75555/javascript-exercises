de4df2d9239c435ed8806da96d244b43928a7961
abdul salaam <asalaam75555@gmail.com>
2025-07-26 18:44:23 +0000

    Add changes for 11 assignment in js exercies

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  const ret = ((x - 32) * 5/9) * 10 / 10;
+   return +ret.toFixed(1);
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  const ret = ((x * 9/5) + 32) * 10 / 10;
+  return +ret.toFixed(1);
 };
 
 // Do not edit below this line
