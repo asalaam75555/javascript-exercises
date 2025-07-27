895f1d469a062ad14ef02b6a2f17b779bc60954a
abdul salaam <asalaam75555@gmail.com>
2025-07-27 15:06:04 +0000

    Add the changes fot 16 exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peoples) {
+    let years = 0
+    let index = 0
+    let date = new Date();
+    peoples.forEach((person,pindex) => {
+        let personyears
+        if (person.yearOfDeath === undefined){
+            personyears = date.getFullYear() - person.yearOfBirth;
+        }else{
+            personyears = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (personyears > years){
+            years = personyears 
+            index = pindex
+        }
+    });
+    return peoples[index];
 };
 
 // Do not edit below this line
