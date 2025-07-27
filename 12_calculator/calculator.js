320768a322e214d1682ff48e68d53aaae2bc6631
abdul salaam <asalaam75555@gmail.com>
2025-07-27 06:43:03 +0000

    Add change to complete assignment 12 of top js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, element) => {
+    return acc += element;
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, element) => {
+    return acc *= element;
+  },1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+	while (a > 0){
+    result *= a;
+    a--;
+  }
+  return result;
 };
 
 // Do not edit below this line
