e32b3db09f689a1541faea4c7a22a7ae17645a92
abdul salaam <asalaam75555@gmail.com>
2025-07-27 12:20:22 +0000

    Add changes for exercise 13 and 14 top js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let alphaArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+        'r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+    let arr = str.toLowerCase().split('');
+    let arrW =[];
+    let arrRW = []
+    let rarr = arr.toReversed();
+    arr.forEach(element => {
+        if (alphaArr.includes(element)){
+            arrW.push(element);
+        }
+    });
+    rarr.forEach(element => {
+        if (alphaArr.includes(element)){
+            arrRW.push(element);
+        }
+    })
+    let words = arrW.join();
+    let rwords = arrRW.join();
+    if (words=== rwords){
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let sum =0;
+    if(typeof(number) !== 'number' && isNaN(+number)){
+        return 0;
+    }
+    else if (+number === 0){
+        return sum;
+    }
+    else if (+number === 1){
+        return 1;
+    }
+    else if (number < 0){
+        return 'OOPS'
+    }   
+        return sum = fibonacci(number - 1)+ fibonacci(number - 2);
 };
 
 // Do not edit below this line
